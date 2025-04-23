1895ed85a36b7829286b05338f8d74a7e06b47cb
TheWExploiter <nuggetsebastian90@gmail.com>
2025-04-23 15:04:03 +0200

    Update FortSpam.lua

--- FortSpam.lua
+++ FortSpam.lua
@@ -7,54 +7,11 @@ local Tab = Window:MakeTab({
 	PremiumOnly = false
 })
 
-local jetSpam = false
 local fortSpam = false
-local jetLoop, fortLoop
+local fortLoop
 
 Tab:AddToggle({
-	Name = "Auto Jet Spam (4s)",
-	Default = false,
-	Callback = function(state)
-		jetSpam = state
-		if jetSpam then
-			jetLoop = task.spawn(function()
-				local player = game:GetService("Players").LocalPlayer
-				local leaderstats = player:FindFirstChild("leaderstats")
-				while jetSpam do
-					if leaderstats and leaderstats:FindFirstChild("Glove") then
-						if leaderstats.Glove.Value == "Jet" then
-							local airstrike = game:GetService("ReplicatedStorage"):FindFirstChild("AirStrike")
-							if airstrike then
-								airstrike:FireServer()
-							end
-						else
-							OrionLib:MakeNotification({
-								Name = "Missing Jet Glove",
-								Content = "Equip the 'Jet' glove to use this!",
-								Image = "rbxassetid://7733960981",
-								Time = 3
-							})
-							task.wait(3)
-						end
-					else
-						OrionLib:MakeNotification({
-							Name = "Checking...",
-							Content = "Looking for 'Jet' glove...",
-							Image = "rbxassetid://7734054307",
-							Time = 2
-						})
-					end
-					task.wait(4)
-				end
-			end)
-		else
-			if jetLoop then task.cancel(jetLoop) end
-		end
-	end
-})
-
-Tab:AddToggle({
-	Name = "Auto Fort Spam (3s)",
+	Name = "Auto Fort Spam",
 	Default = false,
 	Callback = function(state)
 		fortSpam = state
@@ -76,7 +33,7 @@ Tab:AddToggle({
 								Image = "rbxassetid://7733960981",
 								Time = 3
 							})
-							task.wait(4)
+							task.wait(4.3)
 						end
 					else
 						OrionLib:MakeNotification({
@@ -86,7 +43,7 @@ Tab:AddToggle({
 							Time = 2
 						})
 					end
-					task.wait(3)
+					task.wait(4.3)
 				end
 			end)
 		else
